df350f908b2c3b27cc4a74c56979b852a5992342
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-06 12:56:27 +0900

    feat: Add Main

---
+++ src/components/Layout/Main/Main.tsx
@@ -0,0 +1,51 @@
+import { useAtomValue } from "jotai";
+import { AnimatePresence } from "motion/react";
+import { selectedMenuAtom } from "@/atoms/menuAtom";
+import { TRACKS } from "@/constants/tracks";
+import { EXTERNAL } from "@/constants/external";
+import { Menu } from "@/components/Menu/Menu";
+import { Tracks } from "@/components/Tracks/Tracks";
+import { Lyrics } from "@/components/Lyrics/Lyrics";
+import { External } from "@/components/External/External";
+import { BGLogo } from "@/components/BGLogo/BGLogo";
+import { Visuals } from "@/components/Visuals/Visuals";
+import { VISUALS } from "@/constants/visuals";
+
+export const Main = ({ onTrackClick }: { onTrackClick: (no: string, src: string) => void }) => {
+  const selectedMenu = useAtomValue(selectedMenuAtom);
+
+  return (
+    <main className="relative w-full p-20 font-serif min-h-[calc(100vh-80px)]">
+      <BGLogo />
+      <div className="flex gap-40 items-start z-10">
+        <Menu />
+        
+        <AnimatePresence mode="wait">
+          {selectedMenu === "TLDR" && (
+            <Tracks 
+              key="TLDR" 
+              tracks={TRACKS} 
+              onTrackClick={onTrackClick} 
+            />
+          )}
+
+          {selectedMenu === "externals" && (
+            <External 
+              key="externals"
+              externals={EXTERNAL} />
+          )}
+
+          {selectedMenu === "visuals" && (
+            <Visuals
+              key="visuals"
+              visuals={VISUALS} />
+          )}
+
+  
+            <Lyrics />
+
+        </AnimatePresence>
+      </div>
+    </main>
+  );
+};
