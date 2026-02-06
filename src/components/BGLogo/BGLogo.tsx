d762eeb6c86b0f8e8a0bb34c0fc4966bed6060db
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-06 15:17:33 +0900

    fixed: Fix BGLogo location problem

--- src/components/BGLogo/BGLogo.tsx
+++ src/components/BGLogo/BGLogo.tsx
@@ -6,22 +6,23 @@ import { isPlayingAtom } from "@/atoms/playerAtom";
 export const BGLogo = () => {
     const isDark = useAtomValue(themeAtom);
     const logoSrc = isDark ? "/images/r_ryuji_white.png" : "/images/r_ryuji_black.png";
-
     const isPlaying = useAtomValue(isPlayingAtom);
 
     return (
-        <div className="fixed pointer-events-none select-none z-0 right-[5%] bottom-[8%]">
+        <div className="fixed pointer-events-none select-none z-0 right-24 bottom-28">
             <motion.img
                 src={logoSrc}
                 alt="bg-logo"
-                className="w-[50vw] max-w-[250px] opacity-15"
+                className="w-[150px] md:w-[200px] lg:w-[250px] opacity-15"
                 animate={{
                     rotate: isPlaying ? 360 : -12,
                 }}
                 transition={{
-                    rotate: isPlaying ? { repeat: Infinity, duration: 20, ease: "linear" } : { duration: 0.8 },
+                    rotate: isPlaying 
+                        ? { repeat: Infinity, duration: 20, ease: "linear" } 
+                        : { duration: 0.8 },
                 }}
             />
         </div>
     )
-};      
+};
