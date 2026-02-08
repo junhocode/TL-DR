27db37b1248731e2c24d035a9ce3f5647728d7be
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-08 22:59:11 +0900

    Style: Style Change in Main

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -7,23 +7,24 @@ import { Tracks } from "@/components/Tracks/Tracks";
 import { Lyrics } from "@/components/Lyrics/Lyrics";
 import { External } from "@/components/External/External";
 import { Visuals } from "@/components/Visuals/Visuals";
-import { Archive } from "@/components/Archive/Archive";
+import { Archives } from "@/components/Archives/Archives";
 import { BGLogo } from "@/components/BGLogo/BGLogo";
 import { BackButton } from "@/components/BackButton/BackButton";
 import { TRACKS } from "@/constants/tracks";
 import { EXTERNAL } from "@/constants/external";
 import { VISUALS } from "@/constants/visuals";
 import { MENU } from "@/constants/menu";
+import { ARCHIVES } from "@/constants/archives";
 
 export const Main = ({ onTrackClick }: { onTrackClick: (no: string, src: string) => void }) => {
   const selectedMenu = useAtomValue(selectedMenuAtom);
   const playingNo = useAtomValue(trackIdAtom);
 
   return (
-    <main className="relative w-full pl-12 pr-8 md:px-20 md:py-20 font-serif min-h-[calc(100vh-80px)] overflow-hidden">
+    <main className="relative w-full pl-12 pr-8 md:px-20 md:py-20 min-h-[calc(100vh-80px)] overflow-y-auto ">
       <BGLogo />
 
-      <div className="flex flex-col md:flex-row gap-10 md:gap-40 items-start z-10 relative">
+      <div className="flex flex-col md:flex-row h-auto gap-10 md:gap-40 items-start z-10 relative">
         
         <div className={`py-10 ${selectedMenu ? "hidden md:block" : "block"}`}>
           <Menu menu={MENU} />
@@ -33,7 +34,7 @@ export const Main = ({ onTrackClick }: { onTrackClick: (no: string, src: string)
           {selectedMenu && (
             <motion.div 
               key={selectedMenu}
-              className={`py-10 ${playingNo ? "hidden md:block" : "block"} w-full md:w-auto`}
+              className={`py-9 ${playingNo ? "hidden md:block" : "block"} w-full md:w-auto`}
               initial={{ opacity: 0, x: 20 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: -10 }}
@@ -43,7 +44,7 @@ export const Main = ({ onTrackClick }: { onTrackClick: (no: string, src: string)
               {selectedMenu === "TLDR" && <Tracks tracks={TRACKS} onTrackClick={onTrackClick} />}
               {selectedMenu === "externals" && <External externals={EXTERNAL} />}
               {selectedMenu === "visuals" && <Visuals visuals={VISUALS} />}
-              {selectedMenu === "archive" && <Archive />}
+              {selectedMenu === "archive" && <Archives archives={ARCHIVES}/>}
             </motion.div>
           )}
         </AnimatePresence>
@@ -52,7 +53,7 @@ export const Main = ({ onTrackClick }: { onTrackClick: (no: string, src: string)
           {playingNo && (
             <motion.div
               key="lyrics-container"
-              className="block w-full md:w-auto py-10"
+              className="block w-full md:w-auto xs:py-10"
               initial={{ opacity: 0, x: 20 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: 20 }}
