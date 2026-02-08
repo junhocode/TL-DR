b7c4bfbfe15681d87723c2c12bf0946e31944087
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-08 21:59:27 +0900

    refactor: Refactor Tracks using Button, smart opacity

--- src/components/Tracks/Tracks.tsx
+++ src/components/Tracks/Tracks.tsx
@@ -1,6 +1,7 @@
 import { motion } from "motion/react";
 import { useAtomValue } from "jotai";
 import { trackIdAtom } from "@/atoms/playerAtom";
+import { Button } from "@/components/ui/button";
 import type { TrackMenuProps } from "@/types/track.type";
 
 export const Tracks = ({ tracks, onTrackClick }: TrackMenuProps) => {
@@ -8,25 +9,36 @@ export const Tracks = ({ tracks, onTrackClick }: TrackMenuProps) => {
 
   return (
     <ul className="flex flex-col gap-2">
-      {tracks.map((track, index) => (
-        <motion.li 
-          key={track.id}
-          initial={{ opacity: 0, x: 0 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: index * 0.05 }}
-          onClick={() => onTrackClick(track.id, track.src)}
-          className={`group flex gap-8 cursor-pointer transition-all  ${
-            trackId === track.id ? "text-primary" : "opacity-80 hover:opacity-100"
-          }`}
-        >
-          <span className={`w-6 transition-opacity ${trackId === track.id ? "opacity-100" : "opacity-40 group-hover:opacity-100"}`}>
-            {track.id}
-          </span>
-          <span className={`transition-opacity group-hover:underline tracking-tight ${trackId === track.id ? "opacity-100" : "opacity-40 group-hover:opacity-100"}`}>
-            {track.title}
-          </span>
-        </motion.li>
-      ))}
+      {tracks.map((track, index) => {
+        const isPlaying = trackId === track.id;
+
+        return (
+          <motion.li 
+            key={track.id}
+            initial={{ opacity: 0, x: 0 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: index * 0.05 }}
+            className="group"
+          >
+            <Button
+              variant="ghost"
+              onClick={() => onTrackClick(track.id, track.src)}
+              className={`
+                flex w-full h-auto flex justify-start gap-16 text-base 
+                ${isPlaying ? "opacity-80" : "text-smart-opacity"}
+              `}
+            >
+              <span>
+                {track.id}
+              </span>
+
+              <span>
+                {track.title}
+              </span>
+            </Button>
+          </motion.li>
+        );
+      })}
     </ul>
   );
 };
