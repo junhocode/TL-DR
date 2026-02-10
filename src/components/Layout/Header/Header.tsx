92cd7e15aa755be41d766e2fe57aa4c5fe592192
진준호 <theworstvalentine@gmail.com>
2026-02-10 23:18:48 +0900

    Style: Fix style in Header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,9 +3,11 @@ import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
 
 export const Header = () => {
     return (
-        <header className="w-full flex justify-end p-6 gap-3">
-            <MuteToggle />
-            <ThemeToggle />
-        </header>
+      <header className="
+        w-full flex justify-end gap-3 px-4 py-4 md:p-6">
+        <MuteToggle />
+        <ThemeToggle />
+      </header>
     );
-};
+  };
+  
