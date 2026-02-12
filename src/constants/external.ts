4c0a49552356fa694bce85ab7ef9d90dbbda00f3
진준호 <161407999+junhocode@users.noreply.github.com>
2026-02-12 14:16:45 +0900

    chore: Add youtube link

--- src/constants/external.ts
+++ src/constants/external.ts
@@ -1,6 +1,6 @@
 export const EXTERNAL = [
   { id: "spotify", title: "TL;DR - spotify", link: "https://open.spotify.com/album/2WN4Fs9b0pl5wpTCDjN2B7"},
   { id: "applemusic", title: "TL;DR - apple music", link: "https://music.apple.com/kr/album/tl-dr-ep/1875142390"},
-  { id: "youtube", title: "TL;DR - youtube", link: "" },
+  { id: "youtube", title: "TL;DR - youtube", link: "https://www.youtube.com/watch?v=E1vLeY7zgEM&list=OLAK5uy_n7gxIvsNUIVJBMOFR7oc-4MN93cQhsxGA" },
   { id: "instagram", title: "Instagram", link: "https://www.instagram.com/ryuji.wandelstern/"}
 ];
