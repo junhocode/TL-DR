1d2e3406c91fa3e788dbf77f4bdf0ca6ab070ef4
진준호 <161407999+junhocode@users.noreply.github.com>
2026-02-12 12:02:02 +0900

    chore: Add links for spotify and apple music

--- src/constants/external.ts
+++ src/constants/external.ts
@@ -1,6 +1,6 @@
 export const EXTERNAL = [
-  { id: "spotify", title: "TL;DR - spotify", link: "" },
-  { id: "applemusic", title: "TL;DR - apple music", link: "" },
+  { id: "spotify", title: "TL;DR - spotify", link: "https://open.spotify.com/album/2WN4Fs9b0pl5wpTCDjN2B7"},
+  { id: "applemusic", title: "TL;DR - apple music", link: "https://music.apple.com/kr/album/tl-dr-ep/1875142390"},
   { id: "youtube", title: "TL;DR - youtube", link: "" },
   { id: "instagram", title: "Instagram", link: "https://www.instagram.com/ryuji.wandelstern/"}
 ];
