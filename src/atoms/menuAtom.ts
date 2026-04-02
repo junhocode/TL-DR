a5a7ea4b9f74397d377e67b1264225cb69e31947
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-02 15:23:13 +0900

    refactor: Remove unnecessary atoms

--- src/atoms/menuAtom.ts
+++ src/atoms/menuAtom.ts
@@ -1,5 +1,3 @@
 import { atom } from "jotai";
 
 export const selectedMenuAtom = atom<string | null>(null);
-
-export const hasMenuAtom = atom((get) => get(selectedMenuAtom) !== null);
--- src/atoms/playerAtom.ts
+++ src/atoms/playerAtom.ts
@@ -1,5 +1,3 @@
 import { atom } from "jotai";
 
 export const trackIdAtom = atom<string | null>(null);
-
-export const isPlayingAtom = atom((get) => get(trackIdAtom) !== null);
