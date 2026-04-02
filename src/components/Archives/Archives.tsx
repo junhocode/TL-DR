932727086fd69627ae194e12e896c16b6e3e207e
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-02 15:26:55 +0900

    refactor: Motion for Archive menu is now its own responsibility

--- src/components/Archives/Archives.tsx
+++ src/components/Archives/Archives.tsx
@@ -1,17 +1,15 @@
+import { motion } from "motion/react";
+import { PAGE_TRANSITION } from "@/constants/motion";
 import type { Archive } from "@/types/archive.type";
 
 export const Archives = ({ archives }: { archives: Archive[] }) => {
   return (
-    <div className="flex flex-col md:flex-row md:flex-wrap gap-8">
-      {archives.map((img, index) => (
-        <div key={index}>
-          <img
-            src={img.src}
-            alt={img.id}
-            className="w-full md:w-64"
-          />
+    <motion.div {...PAGE_TRANSITION} className="flex flex-col md:flex-row md:flex-wrap gap-8">
+      {archives.map((img) => (
+        <div key={img.id}>
+          <img src={img.src} alt={img.id} className="w-full md:w-64" />
         </div>
       ))}
-    </div>
+    </motion.div>
   );
-}
+};
