2ad3abfa2324023db03ce44a0859f64efd3b9926
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-03 14:54:16 +0900

    style: Remove bg accent from Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
-          "hover:text-accent-foreground dark:hover:bg-accent/50",
+          "hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
